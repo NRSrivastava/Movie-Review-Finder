6fa1290f7443887aa536938f05975e3f2b476112
Neelesh Ranjan Srivastava <neel.17nov@gmail.com>
2021-02-02 14:17:23 +0530

    similar functionality combined.

--- src/main/java/srivastava/neelesh/imdbMovieRatingResolver/resolve.java
+++ src/main/java/srivastava/neelesh/imdbMovieRatingResolver/resolve.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class resolve {
     public static void main(String[]args) throws Exception{
         try{
-        System.out.println("Enter the name of the Movie");
+        System.out.println("Enter the name of the Movie/Series");
         Scanner sc=new Scanner(System.in);
         
         String name=sc.nextLine();
@@ -18,22 +18,8 @@ public class resolve {
         }
         
         String url = "https://sg.media-imdb.com/suggests/"+name.charAt(0)+"/"+name+".json";
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
-        con.setRequestMethod("GET");
-        //add request header
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        //int responseCode = con.getResponseCode();
-        //System.out.println("\nSending 'GET' request to URL : " + url);
-        //System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-           response.append(inputLine);
-        }
-        in.close();
+        
+        String response=getRawString(url);
         
         String jsonString=response.substring(response.indexOf("(")+1,response.lastIndexOf(")"));
         
@@ -69,25 +55,13 @@ public class resolve {
         System.out.println("It may take a moment to load due to bandwidth limitation on IMDb-API's end . . .\n\nRatings:");
         
         url = "https://imdb-api.com/en/API/Ratings/k_loj126o8/"+movieID; //It has my api-key, please generate your own api key on https://imdb-api.com for your projects.
-        obj = new URL(url);
-        con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        //add request header
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        /*responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);*/
-        in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-           response.append(inputLine);
-        }
-        in.close();
+        
+        response=getRawString(url);
         
         
         //System.out.println(response);
         
-        JSONObject json2 = new JSONObject(response.toString());
+        JSONObject json2 = new JSONObject(response);
         String imdb = json2.getString("imDb");
         String metacritic=json2.getString("metacritic");
         String tmdb=json2.getString("theMovieDb");
@@ -106,5 +80,24 @@ public class resolve {
             return a+": "+b+"\n";
         else
             return "";
+    }
+    static String getRawString(String url) throws Exception{
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        //int responseCode = con.getResponseCode();
+        //System.out.println("\nSending 'GET' request to URL : " + url);
+        //System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+           response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
     }
 }
